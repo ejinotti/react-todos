07ae32a5ef016bc742763f5f20ad8bf554f11552
Edward J. Jinotti <ejinotti2@gmail.com>
2016-01-09 23:47:26 -0500

    Some incomplete stuff.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './app/todo_react.jsx',
+  entry: './frontend/todo_react.jsx',
   output: {
-    path: path.join(__dirname, 'static'),
+    path: path.join(__dirname, 'todosapp', 'static'),
     filename: 'bundle.js'
   },
   module: {
