cdbc3121a191942c90c14f3444a4fa25fcb7468c
Edward J. Jinotti <ejinotti2@gmail.com>
2016-01-09 22:47:18 -0500

    Handle result not found erros with json 404.

--- todosapp/__init__.py
+++ todosapp/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
 
 from .routes import add_routes
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+@app.errorhandler(NoResultFound)
+def no_result_handler(e):
+    response = jsonify(message='Not Found')
+    response.status_code = 404
+    return response
 
 add_routes(app)
 
