7b2e4dcd1fb31f21011b9b06a88670cee703fcbb
Edward J. Jinotti <ejinotti2@gmail.com>
2016-01-26 16:38:04 -0500

    Add detail view toggle.

---
+++ frontend/todo_detail_view.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+var TodoStore = require('./stores/todo_store');
+
+module.exports = React.createClass({
+  handleDestroy: function () {
+    TodoStore.destroy(this.props.todo.id);
+  },
+  render: function () {
+    return (
+      <div>
+        <div>{this.props.todo.body}</div>
+        <button onClick={this.handleDestroy}>Delete</button>
+      </div>
+    );
+  }
+});
--- frontend/todo_list_item.jsx
+++ frontend/todo_list_item.jsx
@@ -1,18 +1,28 @@
 var React = require('react');
 
-var TodoStore = require('./stores/todo_store');
 var DoneButton = require('./done_button');
+var TodoDetailView = require('./todo_detail_view');
 
 module.exports = React.createClass({
-  handleDestroy: function () {
-    TodoStore.destroy(this.props.todo.id);
+  getInitialState: function () {
+    return {showDetail: false};
+  },
+  toggleDetail: function () {
+    this.setState({showDetail: !this.state.showDetail});
   },
   render: function () {
+    var detail = '';
+
+    if (this.state.showDetail) {
+      detail = <TodoDetailView todo={this.props.todo} />;
+    }
+
     return (
       <div>
-        <div>{this.props.todo.title}</div>
-        <div>{this.props.todo.body}</div>
-        <button onClick={this.handleDestroy}>Delete</button>
+        <div onClick={this.toggleDetail}>
+          {this.props.todo.title}
+        </div>
+        {detail}
         <DoneButton todo={this.props.todo} />
       </div>
     );
